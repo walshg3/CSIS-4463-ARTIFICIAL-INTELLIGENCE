ec98329eea5c664bcbfbcb0d2d052df67483a910
Greg Walsh <walshg3@go.stockton.edu>
2019-03-26 13:44:06 -0400

    Getting ready for final submission

--- Homework 8/SudokuSolver2.py
+++ Homework 8/SudokuSolver2.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
+#   @author Brain Fox and Greg Walsh
+#   ______      _              ______                          _   _____                  _    _       _     _     
+#    ___ \    (_)             |  ___|                        | | |  __ \                | |  | |     | |   | |    
+#  | |_/ /_ __ _  __ _ _ __   | |_ _____  __   __ _ _ __   __| | | |  \/_ __ ___  __ _  | |  | | __ _| |___| |__  
+#  | ___ \ '__| |/ _` | '_ \  |  _/ _ \ \/ /  / _` | '_ \ / _` | | | __| '__/ _ \/ _` | | |/\| |/ _` | / __| '_ \ 
+#  | |_/ / |  | | (_| | | | | | || (_) >  <  | (_| | | | | (_| | | |_\ \ | |  __/ (_| | \  /\  / (_| | \__ \ | | |
+#  \____/|_|  |_|\__,_|_| |_| \_| \___/_/\_\  \__,_|_| |_|\__,_|  \____/_|  \___|\__, |  \/  \/ \__,_|_|___/_| |_|
+#                                                                                 __/ |                           
+#                                                                                |___/                             
 from itertools import permutations
 import math
-
+# Counter used for counting backtracking
 counter =0 
 def make_constraints(constraint_num):
+        """
+        Constrains are that of sudoku rules:
+        1-9 in row 
+        1-9 in column
+        1-9 in the 3x3 matrix 
+        """
         if constraint_num <9:
             return [9*constraint_num+i for i in range(9)]
         elif(constraint_num<18):
@@ -24,6 +39,9 @@ class BoardState:
             self.puzzlelist= puzzle
     
     def copy(self):
+        """
+        Copies the Boardstates puzzlelist, availability_list, and assigned list
+        """
         temp = BoardState(False)
         temp.puzzlelist = self.puzzlelist[:]
         temp.availability_lists = [alist[:] for alist in self.availability_lists]
@@ -31,14 +49,20 @@ class BoardState:
         return temp
 
 
-#forward checking
 def forward_check(board):
+    """
+    Forward checks the board and adds cell to the assigned list
+    Input: board
+    """
     for i,cell in enumerate(board.puzzlelist):
         if cell != '0':
             board.assigned.add(i)
             propogate(board,i)
 
 def find_conflict(board,cell):
+    """
+    locates conflicts in constraints
+    """
     for constraint in constraints:
         if cell in constraint:
             #check for conflict
@@ -52,6 +76,9 @@ def find_conflict(board,cell):
     return False
 
 def propogate(board,cell):
+    """
+
+    """
    
     for constraint in constraints:
         if cell in constraint:
@@ -63,8 +90,10 @@ def propogate(board,cell):
                     board.availability_lists[other_cell].remove(board.puzzlelist[cell])
 #DFS
 def visit(board,index):
+    """
+
+    """
     global counter
-    
     #for each value
     #assign 
     if(index == 81):
@@ -73,13 +102,7 @@ def visit(board,index):
     if index in board.assigned:
         return visit(board,index+1)
 
-    #print(index)
-    #print(board.availability_lists[index])
-    #print("\t"+str(board.availability_lists[16]))
     for value in board.availability_lists[index]:
-        #print("-----")
-        #print(index)
-        #print(value)
         nextboard = board.copy()
         nextboard.puzzlelist[index] = value
         nextboard.assigned.add(index)
@@ -95,6 +118,11 @@ def visit(board,index):
     return 0
 
 def solve(name):
+    """
+    Solves the Puzzle using forward_check() and visit()
+    Input: name of puzzle from sudokus (Without ".txt")
+    returns list of solved Puzzle
+    """
     puzzlelist = []
     puzzle = open("sudokus/"+name+".txt")
 
@@ -105,22 +133,29 @@ def solve(name):
     board=BoardState(puzzlelist)
     forward_check(board)
     return visit(board,0).puzzlelist
-    
+
+def output(puzzle):
+    '''
+    Outputs puzzle in 9x9 format
+    Input: Puzzle (Solved Puzzle)
+    '''
+    n = m = 0
+    while m < len(puzzle):
+        m = m+9
+        print(" ".join(puzzle[n:m]))
+        n = m
 
 def puzzle_check(puzzle, solution):
     '''
     Checks to see if the puzzle is the correct solution
     Arguments:
-        puzzle:  puzzle to be checked
+        puzzle: puzzle to be checked
         solution: solution file (.txt)
     Outputs: True or False 
     '''
     solutionfile = open("solutions/"+solution+"_s.txt")
     solutionList = []
-    #head = [next(solutionfile) for x in range(12)]
-    #print(head)
     for row in solutionfile:
-        #print(row[:22])
         solutionList.append([cell for cell in row[:22].split()])
     solutionfile.close()
     # Start Deleting Line not needed 
@@ -155,14 +190,15 @@ def puzzle_check(puzzle, solution):
     for row in solutionList:
         for cell in row:
             checklist.append(cell)
-    print(checklist)
-    print(solution)
+    # Check if the AI's solution is the actual solution
     for i, item in enumerate(checklist):
         if puzzle[i] != item:
             return False
     return True
 
-#puzzle_name = input()
-print(solve("hard"))
+print("Hello!", "\n", "Please Enter Sudoku puzzle to solve (You can find available Sudoku in the sudokus folder)")
+puzzle_name = input()
+output(solve(puzzle_name))
+#output(solve("hard"))
 #print(puzzle_check(solve(puzzle_name), puzzle_name))i
-print(counter)
+#print("\n",counter)
